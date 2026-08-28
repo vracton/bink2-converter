c44637488e3dfb3067b5ae8fbbab6ad955bed170
vracto <vracto@icloud.com>
2026-08-28 11:52:18 -0500

    Expose OPFS AVIO lifecycle helpers

--- src/opfs_io.h
+++ src/opfs_io.h
@@ -3,9 +3,10 @@
 
 #include <libavformat/avformat.h>
 
+int bink2_opfs_is_active(void);
 int bink2_opfs_open_input(AVFormatContext **ctx, const char *label);
-int bink2_opfs_attach_output(AVFormatContext *ctx);
+int bink2_opfs_open_output(AVIOContext **pb);
 void bink2_opfs_close_input(AVFormatContext **ctx);
-void bink2_opfs_close_output(AVFormatContext *ctx);
+int bink2_opfs_close_output(AVIOContext **pb);
 
 #endif
