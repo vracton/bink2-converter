48e93c2513ee7a492af53f7f95fdec56bf3929cb
vracto <vracto@icloud.com>
2026-08-27 23:49:11 -0500

    Reduce temporary FFmpeg logging overhead

--- src/debug_trace.c
+++ src/debug_trace.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <libavutil/log.h>
 
-/* TEMPORARY DEBUG BUILD: make FFmpeg emit its most verbose runtime trace.
-   Remove this file from the link command once the startup stall is identified. */
+/* TEMPORARY DEBUG BUILD: keep useful FFmpeg diagnostics without the AV_LOG_TRACE
+   firehose, which can itself dominate browser time when forwarded to the UI. */
 __attribute__((constructor))
 static void bink2_enable_ffmpeg_trace(void) {
-    av_log_set_level(AV_LOG_TRACE);
+    av_log_set_level(AV_LOG_VERBOSE);
     av_log_set_flags(AV_LOG_PRINT_LEVEL);
-    fprintf(stderr, "[bink2-debug] FFmpeg AV_LOG_TRACE enabled\n");
+    fprintf(stderr, "[bink2-debug] FFmpeg AV_LOG_VERBOSE enabled\n");
     fflush(stderr);
 }
